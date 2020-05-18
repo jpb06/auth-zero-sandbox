e08e2332170ddb67d7ad2ca1dcef2cdf8f2a7100
jpb06 <jpb.06@outlook.com>
2020-05-18 12:30:31 +0200

    Middleware checking if the caller is authenticated

---
+++ api/src/middleware/is.authenticated.middleware.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from "express";
+import * as jwt from "express-jwt";
+import * as jwksRsa from "jwks-rsa";
+
+const isAuthenticated = jwt({
+  // Dynamically provide a signing key based on the kid in the header
+  // and the signing keys provided by the JWKS endpoint.
+  secret: jwksRsa.expressJwtSecret({
+    cache: true, // cache the signing key
+    rateLimit: true,
+    jwksRequestsPerMinute: 5, // prevent attackers from requesting more than 5 per minute
+    jwksUri: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/.well-known/jwks.json`,
+  }),
+
+  // Validate the audience and the issuer.
+  audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+  issuer: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/`,
+
+  // This must match the algorithm selected in the Auth0 dashboard under your app's advanced settings under the OAuth tab
+  algorithms: ["RS256"],
+});
+
+export default isAuthenticated;
