a3fbf17c6e906ad3cce94a38f184103f249f6431
jpb06 <jpb.06@outlook.com>
2020-05-18 12:32:24 +0200

    Redux : store

---
+++ front-react/src/redux/store/root.state.ts
@@ -0,0 +1,37 @@
+import { recoverSession } from "../../logic/session.logic";
+import { Session } from "../../types/session.type";
+import { UserProfile } from "../../types/user.profile";
+import { Repo } from "../../types/repo.type";
+import {
+  CustomSnackbarData,
+  SnackbarType,
+} from "../../components/generic/CustomSnackbar";
+
+export enum AppStatus {
+  BusyRetrievingProfile,
+  BusyRetrievingTimeline,
+  BusyLoggingIn,
+  BusyMakingRestrictedApiCalls,
+  Available,
+  Errored,
+}
+
+export interface RootState {
+  readonly appStatus: AppStatus;
+  readonly snackBar: CustomSnackbarData;
+  readonly session: Session | null;
+  readonly userProfile: UserProfile | null;
+  readonly timeline: Array<Repo>;
+}
+
+export const initialState: RootState = {
+  appStatus: AppStatus.Available,
+  snackBar: {
+    isOpen: false,
+    type: SnackbarType.error,
+    text: "",
+  },
+  session: recoverSession(),
+  userProfile: null,
+  timeline: [],
+};
