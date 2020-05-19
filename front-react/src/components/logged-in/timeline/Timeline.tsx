dadae6d1c3883513792f5d936a4a137c38395a75
jpb06 <jpb.06@outlook.com>
2020-05-19 12:02:14 +0200

    Fixing a little issue with timeline: the loading action was still present after the call failed

--- front-react/src/components/logged-in/timeline/Timeline.tsx
+++ front-react/src/components/logged-in/timeline/Timeline.tsx
@@ -4,17 +4,28 @@ import styles from "../LoggedInPage.styles";
 import RepoComponent from "./repo/Repo";
 import useTimelineLoading from "../../../hooks/use.timeline.loading.hook";
 import Busy from "../../generic/busy/Busy";
+import { useReduxSelector } from "../../../hooks/redux.hooks";
+import { AppStatus } from "../../../redux/store/root.state";
 
 const Timeline = () => {
   const classes = styles();
   const timeline = useTimelineLoading();
+  const isReady = useReduxSelector(
+    (state) => state.appStatus === AppStatus.Available
+  );
 
   return (
     <>
       <BreadCrumb text="Timeline" />
       <div className={classes.root}>
         {timeline.length === 0 ? (
-          <Busy text="Loading your timeline" />
+          isReady ? (
+            <div>
+              Ooops! Looks like we weren't able to retrieve your timeline.
+            </div>
+          ) : (
+            <Busy text="Loading your timeline" />
+          )
         ) : (
           timeline.map((repo) => <RepoComponent key={repo.id} repo={repo} />)
         )}
