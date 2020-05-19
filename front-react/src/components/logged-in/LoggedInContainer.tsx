18c2174c7bc509cd3b018ac9772111ecc2cb3f2a
jpb06 <jpb.06@outlook.com>
2020-05-19 11:59:41 +0200

    On loading a logged in route, checking if session is recoverable

--- front-react/src/components/logged-in/LoggedInContainer.tsx
+++ front-react/src/components/logged-in/LoggedInContainer.tsx
@@ -1,9 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Nav from "./generic/menu/Nav";
-import { useReduxSelector } from "../../hooks/redux.hooks";
+import { useReduxSelector, useReduxDispatch } from "../../hooks/redux.hooks";
 import { useHistory } from "react-router-dom";
 import { AppStatus } from "../../redux/store/root.state";
-import Busy from "../generic/busy/Busy";
+import { action } from "../../redux/actions/generic.actions";
+import { LOGGED_IN } from "../../types/redux/action.types";
+import AuthLogic from "../../logic/auth.logic";
+import GloballyBusy from "./generic/globally-busy/GloballyBusy";
 
 interface LoggedInContainerProps {
   Component: React.ElementType;
@@ -13,18 +16,37 @@ const LoggedInContainer: React.FC<LoggedInContainerProps> = ({
   Component,
   ...rest
 }) => {
+  const history = useHistory();
+  const dispatch = useReduxDispatch();
   const session = useReduxSelector((state) => state.session);
   const appStatus = useReduxSelector((state) => state.appStatus);
-  const history = useHistory();
+
+  const [sessionRenewalAttempted, setSessionRenewalAttempted] = useState(false);
 
   useEffect(() => {
-    if (appStatus === AppStatus.Available && !session) {
-      history.push("/");
-    }
-  }, [history, session, appStatus]);
+    const renew = async () => {
+      setSessionRenewalAttempted(true);
+      console.log("Attempting to restore session");
+      try {
+        const result = await AuthLogic.renewToken().then(
+          (x) =>
+            new Promise<any>((resolve) => setTimeout(() => resolve(x), 1500))
+        );
+        dispatch(action(LOGGED_IN, result));
+        console.log("session recovered");
+      } catch (err) {
+        history.push("/");
+      }
+    };
+
+    if (!session && !sessionRenewalAttempted) renew();
+  }, [dispatch, session, sessionRenewalAttempted, history]);
 
   if (appStatus === AppStatus.BusyLoggingIn) {
-    return <Busy text="Login in progress... Please wait" />;
+    return <GloballyBusy text="Login in progress... Please wait" />;
+  }
+  if (sessionRenewalAttempted && !session) {
+    return <GloballyBusy text="Attempting to restore your session..." />;
   }
 
   return (
